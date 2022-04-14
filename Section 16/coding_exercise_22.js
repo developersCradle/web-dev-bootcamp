098f46f8ff183e269df28b879f2ee63c89556acf
Heikki Siikavirta <heikki.siikavirta@gmail.com>
2022-04-14 22:34:00 +0300

    finished chapter 16

--- Section 16/coding_exercise_22.js
+++ Section 16/coding_exercise_22.js
@@ -1,5 +1,15 @@
-function isEven(num) {
-    if (num % 2 === 0) {
-        console.log("even");
-    }
+function getColor(phrase){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓
+   if(phrase === 'stop'){
+       console.log('red');
+   }
+   else if (phrase === 'slow'){
+       console.log('yellow');
+   }
+   else if (phrase === 'go'){
+       console.log('green');
+   }
+   else {
+       console.log('purple');
+   }
 }
---
+++ Section 16/coding_exercise_24.js
@@ -0,0 +1,10 @@
+const mystery = 'People7'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+
+
+// LEAVE THIS CODE ALONE! (pretty please)
+
+if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
+
+    console.log("YOU GOT IT!!!");
+
+}
